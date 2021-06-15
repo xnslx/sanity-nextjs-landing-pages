0177a6dc935ab0de37afb6e6197c8d3f002b5833
xian <xnslx89@gmail.com>
2021-06-14 21:26:07 -0500

    progress frontend

--- web/components/sections/ProgressSection.js
+++ web/components/sections/ProgressSection.js
@@ -1,32 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import client from '../../client'
-import imageUrlBuilder from '@sanity/image-url'
+import React from "react";
+import PropTypes from "prop-types";
+import client from "../../client";
+import imageUrlBuilder from "@sanity/image-url";
 
-const builder = imageUrlBuilder(client)
+const builder = imageUrlBuilder(client);
 
 const ProgressSection = (props) => {
-  const {heading, label, image, subheading, sublabel} = props
+  const { heading, label, steps } = props;
 
   return (
     <div>
       <h1>{heading}</h1>
       <h3>{label}</h3>
       <div>
+        {steps.map((step) => (
+          <ul>
+            <li>
+              <img src={builder.image(step.image.asset._ref).width(48)} />
+            </li>
+            <li>{step.subheading}</li>
+            <li>{step.sublabel}</li>
+          </ul>
+        ))}
+      </div>
+      {/* <div>
         <img src={builder.image(image.asset._ref).width(48)} />
         <h5>{subheading}</h5>
         <p>{sublabel}</p>
-      </div>
+      </div> */}
     </div>
-  )
-}
+  );
+};
 
 ProgressSection.propTypes = {
   heading: PropTypes.string,
   label: PropTypes.string,
-  subheading: PropTypes.string,
-  image: PropTypes.object,
-  sublabel: PropTypes.string
-}
+  steps: PropTypes.array,
+};
 
-export default ProgressSection
+export default ProgressSection;
